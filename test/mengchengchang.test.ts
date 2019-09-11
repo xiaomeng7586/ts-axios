c44d3a43626c610b8945beb05f228769d222e58b
mengcc <758690263@qq.com>
2019-09-11 10:40:44 +0800

    初始化项目

---
+++ test/mengchengchang.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/mengchengchang"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
